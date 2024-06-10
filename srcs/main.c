010beab34a84c3ba53ccffff5b8de60299514c5b
Bransilav Bogdanovic <bbogdano@c2r7s7.42wolfsburg.de>
2024-06-10 10:15:37 +0200

    overwrite

--- srcs/main.c
+++ srcs/main.c
@@ -6,27 +6,38 @@
 /*   By: bbogdano <bbogdano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 13:03:52 by bbogdano          #+#    #+#             */
-/*   Updated: 2024/06/08 01:34:53 by bbogdano         ###   ########.fr       */
+/*   Updated: 2024/06/10 10:14:01 by bbogdano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static void	validate_arguments(int argc, char **argv)
-{
-	if (argc != 2)
-		handle_error("Usage: ./so_long <map_file.ber>");
-	check_file(argv[1]);
-}
+#include "so_long.h"
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	t_game	game;
+    t_game game;
+    size_t rows, cols;
 
-	validate_arguments(argc, argv);
-	if (!init_game(&game, argv[1]))
-		handle_error("Error: Failed to initialize game.");
-	//gameloop
-	cleanup_game(&game);
-	return (0);
+    if (argc != 2)
+        handle_error("Usage: ./so_long <map_file>");
+
+    game.map = read_map(argv[1], &rows, &cols);
+    validate_map(game.map, rows, cols);
+
+    // Initialize MiniLibX and create a window
+    game.mlx = mlx_init();
+    if (!game.mlx)
+        handle_error("Error: Failed to initialize MiniLibX");
+    game.win = mlx_new_window(game.mlx, cols * TILE_SIZE, rows * TILE_SIZE, "So Long");
+    if (!game.win)
+        handle_error("Error: Failed to create window");
+
+    // Render the map (placeholder, no actual rendering logic yet)
+    // ...
+
+    // Start the MiniLibX event loop
+    mlx_loop(game.mlx);
+
+    return 0;
 }
